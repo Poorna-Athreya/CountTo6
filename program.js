f1d0d5b4e5b72bc9cbc26849a1671468d4dec4ad
Poorna-Athreya <Poorna_athreya@mckinsey.com>
2022-02-12 14:01:10 +0530

    Feat: Arrow function exec

--- program.js
+++ program.js
@@ -1,3 +1,9 @@
 // console.log('HELLO ES6')
-console.log(`Hello, ${process.argv[2]}!
-Your name lowercased is "${process.argv[2].toLowerCase()}".`)
+// console.log(`Hello, ${process.argv[2]}!
+// Your name lowercased is "${process.argv[2].toLowerCase()}".`)
+var inputs = process.argv.slice(2)
+const   result = inputs.map(str => str.charAt(0))
+        .reduce((finalString, str) => {
+            return `${finalString}${str}`;
+        }, '');
+        console.log(`[${inputs}] becomes "${result}"`);
