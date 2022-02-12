47d4851d198f16e5a00ef7c7093d356715a876ca
Poorna-Athreya <Poorna_athreya@mckinsey.com>
2022-02-12 14:05:16 +0530

    Feat: Arrow function with this pointer

--- program.js
+++ program.js
@@ -1,9 +1,23 @@
 // console.log('HELLO ES6')
+
 // console.log(`Hello, ${process.argv[2]}!
 // Your name lowercased is "${process.argv[2].toLowerCase()}".`)
-var inputs = process.argv.slice(2)
-const   result = inputs.map(str => str.charAt(0))
-        .reduce((finalString, str) => {
-            return `${finalString}${str}`;
-        }, '');
-        console.log(`[${inputs}] becomes "${result}"`);
+
+// var inputs = process.argv.slice(2)
+// const   result = inputs.map(str => str.charAt(0))
+//         .reduce((finalString, str) => {
+//             return `${finalString}${str}`;
+//         }, '');
+//         console.log(`[${inputs}] becomes "${result}"`);
+
+const foot = {
+    kick() {
+        this.yelp = 'Ouch!';
+
+        setImmediate(() => {
+            console.log(this.yelp);
+        });
+    },
+};
+
+foot.kick();
