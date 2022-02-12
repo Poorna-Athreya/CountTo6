46a6e2e74a1cc0271bf965c776e63a3f99c1b8a9
Poorna-Athreya <Poorna_athreya@mckinsey.com>
2022-02-12 14:50:22 +0530

    Feat: Template strings to display comment

--- program.js
+++ program.js
@@ -44,6 +44,19 @@
 // };
 
 
-module.exports = function makeImportant(stringInput, exclamations = stringInput.length) {
-    return stringInput + '!'.repeat(exclamations)
+// module.exports = function makeImportant(stringInput, exclamations = stringInput.length) {
+//     return stringInput + '!'.repeat(exclamations)
+// }
+
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+function html(stringMsg, name, message) {
+    const escaped = message
+        .replace(/&/g, '&amp;')
+        .replace(/'/g, '&apos;')
+        .replace(/"/g, '&quot;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+
+    return stringMsg[0] + name + stringMsg[1] + escaped + stringMsg[2];
 }
